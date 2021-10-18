f7dffc996af6abc03baeff12c8f62ac1fe1e5481
ryan gendel <ryangendel@ryans-MacBook-Pro.local>
2021-10-18 12:42:54 -0600

    message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  let [data, setData]=useState()
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    fetch("https://backendapi5.herokuapp.com")
+    .then((data)=> data.json())
+    .then((data)=>{
+      console.log("+++++++")
+      console.log(data)
+      console.log("+++++++")
+    })
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
